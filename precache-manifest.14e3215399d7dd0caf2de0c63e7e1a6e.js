0d0fffee28a44c9b3629010d28fe3ce16b9e685d
Вася Иванов <soldatik2005@mail.ru>
2020-06-11 00:30:59 +0300

    Updates

---
+++ precache-manifest.14e3215399d7dd0caf2de0c63e7e1a6e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1942d6da168a97314660962313f00ce",
+    "url": "/vc-test/index.html"
+  },
+  {
+    "revision": "c4d1fdeb3910a913a444",
+    "url": "/vc-test/static/css/main.dcc97520.chunk.css"
+  },
+  {
+    "revision": "e5a967d0e4d052981415",
+    "url": "/vc-test/static/js/2.1b5ba10f.chunk.js"
+  },
+  {
+    "revision": "8405fc7db03aa55859ceaaa8183fa901",
+    "url": "/vc-test/static/js/2.1b5ba10f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d1fdeb3910a913a444",
+    "url": "/vc-test/static/js/main.ebfae2bf.chunk.js"
+  },
+  {
+    "revision": "ee565f3e1d2247e5327e",
+    "url": "/vc-test/static/js/runtime-main.00d23b72.js"
+  },
+  {
+    "revision": "913bae141f6a1efa3888b865519625e2",
+    "url": "/vc-test/static/media/arrow-right.913bae14.svg"
+  },
+  {
+    "revision": "1565a9de7f4ae4684154162600b80296",
+    "url": "/vc-test/static/media/bg.1565a9de.png"
+  },
+  {
+    "revision": "7bfc346281840fb91226270a2597a1e7",
+    "url": "/vc-test/static/media/img01.7bfc3462.png"
+  },
+  {
+    "revision": "acc99f932868582a914f7aef0355f0ce",
+    "url": "/vc-test/static/media/img02.acc99f93.png"
+  },
+  {
+    "revision": "4cf82839a14a94692a17df4240fe04ad",
+    "url": "/vc-test/static/media/img03.4cf82839.png"
+  },
+  {
+    "revision": "d6c155b3985ea3f95c958ba2568020ee",
+    "url": "/vc-test/static/media/refresh-arrow.d6c155b3.svg"
+  },
+  {
+    "revision": "5e9f3a21f3eb04b624b2d74f5539fa6b",
+    "url": "/vc-test/static/media/score0.5e9f3a21.png"
+  },
+  {
+    "revision": "9a71676ca333f174dd1bcb1423a9980b",
+    "url": "/vc-test/static/media/score3.9a71676c.png"
+  },
+  {
+    "revision": "8aaf4d46e32b7d1acb2f3b9454e8b398",
+    "url": "/vc-test/static/media/score5.8aaf4d46.png"
+  },
+  {
+    "revision": "76bff40f2dbad313162a2be878599fba",
+    "url": "/vc-test/static/media/score7.76bff40f.png"
+  },
+  {
+    "revision": "5956537016711c5cc0f66e471d94cff7",
+    "url": "/vc-test/static/media/score8.59565370.png"
+  }
+]);
